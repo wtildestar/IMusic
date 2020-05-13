88f410f43fc282398367a90bfbd347d1ef51532c
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-05-14 02:53:37 +0500

    Fetch data with AF

---
+++ IMusic/Controllers/SearchViewController.swift
@@ -0,0 +1,82 @@
+//
+//  SearchViewController.swift
+//  IMusic
+//
+//  Created by wtildestar on 13/05/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+struct TrackModel {
+  var trackName: String
+  var artistName: String
+}
+
+final class SearchViewController: UITableViewController {
+  // MARK: - Variables
+  var tracks = [Track]()
+  private var timer: Timer?
+  let searchController = UISearchController(searchResultsController: nil)
+  
+  // MARK: - ViewController
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    setupSearchBar()
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SearchViewController")
+  }
+  
+  // MARK: - Methods
+  private func setupSearchBar() {
+    navigationItem.searchController = searchController
+    searchController.searchBar.delegate = self
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    tracks.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "SearchViewController", for: indexPath)
+    let track = tracks[indexPath.row]
+    cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+    cell.textLabel?.numberOfLines = 2
+    cell.imageView?.image = UIImage(named: "image")
+    return cell
+  }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    print(searchText)
+    timer?.invalidate()
+    
+    timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+      let url = "https://itunes.apple.com/search?term=\(searchText)"
+      let parameters = ["term": "\(searchText)",
+        "limit": "10"]
+      
+      AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).responseData { dataResponse in
+        if let error = dataResponse.error {
+          print("Error received requestion data: \(error.localizedDescription)")
+          return
+        }
+        
+        guard let data = dataResponse.data else { return }
+        
+        let decoder = JSONDecoder()
+        do {
+          let objects = try decoder.decode(Search.self, from: data)
+          print("objects", objects)
+          self.tracks = objects.results
+          self.tableView.reloadData()
+        } catch let jsonError {
+          print("Failed to decode json", jsonError)
+        }
+      }
+    })
+    
+  }
+}
---
+++ IMusic/Models/Search.swift
@@ -0,0 +1,19 @@
+//
+//  Search.swift
+//  IMusic
+//
+//  Created by wtildestar on 14/05/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+struct Search: Decodable {
+  var resultCount: Int
+  var results: [Track]
+}
+
+struct Track: Decodable {
+  var trackName: String
+  var collectionName: String?
+  var artistName: String
+  var artworkUrl100: String?
+}
